ab93b5b587380cf20a4f8b4ccf0510670815adeb
iman <dev.imansabet@gmail.com>
2025-03-19 17:38:14 +0330

    Add Business Rules / Logic :  - record represents a temperature value with specific constraints - record encapsulates a ushort value that represents the number of travel days

---
+++ SimpleTravelManagement.Domain/Exceptions/InvalidTemperatureException.cs
@@ -0,0 +1,15 @@
+﻿using SimpleTravelManagement.Shared.Abstractions.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTravelManagement.Domain.Exceptions;
+
+public  class InvalidTemperatureException : TravelerCheckListException
+{
+    public double Value { get;  }
+    public InvalidTemperatureException(double value) : base($"Value : `{value}` is invalid temperature") => Value = value;
+    
+}
---
+++ SimpleTravelManagement.Domain/Exceptions/InvalidTravelDaysException.cs
@@ -0,0 +1,14 @@
+﻿using SimpleTravelManagement.Shared.Abstractions.Exceptions;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SimpleTravelManagement.Domain.Exceptions;
+
+public class InvalidTravelDaysException : TravelerCheckListException
+{
+    public ushort Days { get; }
+    public InvalidTravelDaysException(ushort days) : base($"Value '{days}' is Invalid ") => 
+       Days = days;
+     
+    
+}
+ 
