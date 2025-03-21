b10121348b15cf06361a5f9ce2a4ccffc76dd145
iman <dev.imansabet@gmail.com>
2025-03-21 12:33:01 +0330

    Add Repository in Domain Proj - Add Application Layer implement CQRS Pattern

---
+++ SimpleTravelManagement.Domain/Repositories/ITravelerCheckListRepository.cs
@@ -0,0 +1,13 @@
+﻿using SimpleTravelManagement.Domain.Entities;
+using SimpleTravelManagement.Domain.ValueObjects;
+
+namespace SimpleTravelManagement.Domain.Repositories;
+
+public interface ITravelerCheckListRepository
+{
+    Task<TravelerCheckList> GetAsync(TravelerCheckListId id);
+    Task AddAsync(TravelerCheckList travelerCheckList );
+    Task UpdateAsync(TravelerCheckList travelerCheckList );
+    Task DeleteAsync(TravelerCheckList travelerCheckList );
+}
+ 
