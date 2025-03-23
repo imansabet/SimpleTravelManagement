a4bbb939179bffeb58ac80d7c7fcc4bafd943646
iman <dev.imansabet@gmail.com>
2025-03-23 13:10:54 +0330

    Add SearchTravelerCheckList Handler

---
+++ SimpleTravelManagement.Infrastructure/EF/Queries/Handlers/SearchTravelerCheckListHandler.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleTravelManagement.Application.DTO;
+using SimpleTravelManagement.Application.Queries;
+using SimpleTravelManagement.Infrastructure.EF.Context;
+using SimpleTravelManagement.Infrastructure.EF.Models;
+using SimpleTravelManagement.Shared.Abstractions.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTravelManagement.Infrastructure.EF.Queries.Handlers
+{
+    internal sealed class SearchTravelerCheckListHandler : IQueryHandler<SearchTravelerCheckList, IEnumerable<TravelerCheckListDto>>
+    {
+        private readonly DbSet<TravelerCheckListReadModel> _TravelerCheckLists;
+
+        public SearchTravelerCheckListHandler(ReadDbContext context)
+            => _TravelerCheckLists = context.TravelerCheckList;
+
+        public async Task<IEnumerable<TravelerCheckListDto>> HandleAsync(SearchTravelerCheckList query)
+        {
+            var dbQuery = _TravelerCheckLists
+                .Include(pl => pl.Items)
+            .AsQueryable();
+
+            if (query.SearchPhrase is not null)
+            {
+                dbQuery = dbQuery.Where(pl =>
+                    Microsoft.EntityFrameworkCore.EF.Functions.Like(pl.Name, $"%{query.SearchPhrase}%"));
+            }
+
+            return await dbQuery
+                .Select(pl => pl.AsDto())
+                .AsNoTracking()
+                .ToListAsync();
+        }
+    }
+}
